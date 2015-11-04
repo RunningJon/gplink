1f9eb8c086bfa168222ef8a2b2af671d6796f8e1
jgronline@gmail.com <jgronline@gmail.com>
2015-11-04 10:33:58 -0600

    0.1.6

---
+++ history/0.1.6/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+echo "Manifest-Version: 1.0" > manifest.txt
+echo "Main-Class: ExternalData" >> manifest.txt
+echo "Specification-Title: \"PivotalGuruGPLink\"" >> manifest.txt
+echo "Specification-Version: \"1.0\"" >> manifest.txt
+echo "Created-By: 1.6.0_65-b14-462-11M4609" >> manifest.txt
+d=`date`
+echo "Build-Date: $d" >> manifest.txt
+
+javac *.java
+jar cfm PivotalGuruGPLink.jar manifest.txt ExternalData.class CommonDB.class SQLFile.class TargetData.class Listener.class Validation.class GPLink.class
+mv PivotalGuruGPLink.jar jar/
